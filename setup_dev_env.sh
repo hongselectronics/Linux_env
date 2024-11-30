17369ef7dc6b71a2a0e948da1a8e807b15aa33b5
Jeremy Hong <jhong@hongseleco.com>
2024-11-30 09:30:14 -0500

    Update setup_dev_env.sh
    add dracula theme

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -7,7 +7,7 @@ setup_ubuntu_debian() {
     # Update and install packages
     sudo apt update && sudo apt upgrade -y
     sudo apt install -y zsh git curl fonts-powerline vim gcc g++ clang python3 python3-pip \
-        build-essential cmake make libtool pkg-config libboost-all-dev
+        build-essential cmake make libtool pkg-config libboost-all-dev dconf-cli
 
     # Install oh-my-zsh
     if ! command -v zsh &> /dev/null; then
@@ -49,7 +49,7 @@ setup_arch() {
     # Update and install packages
     sudo pacman -Syu --noconfirm
     sudo pacman -S --noconfirm zsh git curl powerline-fonts vim gcc clang python python-pip \
-        base-devel cmake libtool boost
+        base-devel cmake libtool boost dconf
 
     # Install oh-my-zsh
     if ! command -v zsh &> /dev/null; then
@@ -84,9 +84,37 @@ EOL
     echo "Arch Linux setup completed!"
 }
 
-# Darcula terminal theme setup (placeholder message)
-setup_darcula_theme() {
-    echo "Please install and set the Darcula terminal theme manually."
+# Dracula theme setup
+setup_dracula_theme() {
+    echo "Setting up Dracula theme for the terminal..."
+
+    # GNOME Terminal
+    if [ "$(basename "$SHELL")" == "bash" ] || [ "$(basename "$SHELL")" == "zsh" ]; then
+        echo "Configuring Dracula theme for GNOME Terminal..."
+        git clone https://github.com/dracula/gnome-terminal ~/dracula-gnome-terminal
+        cd ~/dracula-gnome-terminal
+        ./install.sh
+        cd ..
+        rm -rf ~/dracula-gnome-terminal
+    fi
+
+    # KDE Konsole
+    if command -v konsole &> /dev/null; then
+        echo "Configuring Dracula theme for KDE Konsole..."
+        mkdir -p ~/.local/share/konsole
+        git clone https://github.com/dracula/konsole ~/dracula-konsole
+        cp ~/dracula-konsole/Dracula.colorscheme ~/.local/share/konsole/
+        rm -rf ~/dracula-konsole
+    fi
+
+    # XFCE Terminal
+    if command -v xfce4-terminal &> /dev/null; then
+        echo "Configuring Dracula theme for XFCE Terminal..."
+        mkdir -p ~/.config/xfce4/terminal/colorschemes
+        wget https://raw.githubusercontent.com/dracula/xfce-terminal/master/Dracula.theme -O ~/.config/xfce4/terminal/colorschemes/Dracula.theme
+    fi
+
+    echo "Dracula theme setup completed! Please restart your terminal for changes to take effect."
 }
 
 # Detect OS and run setup
@@ -107,5 +135,5 @@ else
     echo "Unable to detect OS. Make sure you're running a supported Linux distribution."
 fi
 
-# Prompt for Darcula theme
-setup_darcula_theme
+# Setup Dracula theme
+setup_dracula_theme
